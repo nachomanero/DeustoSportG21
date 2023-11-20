6ffdc0ffaf88d33a34d89e2bb701c0905008df2b
InigoDiazMunio <i.diazmunio@opendeusto.es>
2023-11-20 19:46:39 +0100

    cambios en la clase fecha

--- DeustoDeporteG21/src/domain/Fecha.java
+++ DeustoDeporteG21/src/domain/Fecha.java
@@ -1,8 +1,33 @@
 package domain;
 
-public enum Fecha {
-    LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO,
-    ENERO, FEBRERO, MARZO, ABRIL, MAYO, JUNIO, JULIO, AGOSTO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE,
-    ANIO_2021, ANIO_2022, ANIO_2023, ANIO_2024, ANIO_2025,ANIO_2026,ANIO_2027,ANIO_2028,ANIO_2029,ANIO_2030
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fecha {
+
+    public static List<LocalDate> generarFechas(int[] años) {
+        List<LocalDate> fechas = new ArrayList<>();
+
+        for (int año : años) {
+            for (int mes = 1; mes <= 12; mes++) {
+                int diasEnMes = LocalDate.of(año, mes, 1).lengthOfMonth();
+                for (int dia = 1; dia <= diasEnMes; dia++) {
+                    fechas.add(LocalDate.of(año, mes, dia));
+                }
+            }
+        }
+
+        return fechas;
+    }
+
+    public static void main(String[] args) {
+        int[] años = {2023, 2024};
+        List<LocalDate> todasLasFechas = generarFechas(años);
+
+        // Imprime todas las fechas generadas
+        todasLasFechas.forEach(System.out::println);
+    }
 }
+
 
