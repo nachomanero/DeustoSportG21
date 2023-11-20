1d8ef69978fb7e6487f785f48454724fc0d1a922
InigoDiazMunio <i.diazmunio@opendeusto.es>
2023-11-20 18:07:14 +0100

    clase usuario con sus parametros y metodos getters y setters

--- DeustoDeporteG21/src/domain/Usuario.java
+++ DeustoDeporteG21/src/domain/Usuario.java
@@ -2,4 +2,70 @@ package domain;
 
 public class Usuario {
 
+    protected String dni;
+    protected String nombre;
+    protected String apellido;
+    protected String direccion;
+    protected String correoElectronico;
+    protected String contrasena;
+
+    // Constructor de Usuario
+    public Usuario(String dni, String nombre, String apellido, String direccion, String correoElectronico, String contrasena) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.correoElectronico = correoElectronico;
+        this.contrasena = contrasena;
+    }
+
+    // Métodos getter y setter para acceder y modificar los atributos del Usuario
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
 }
