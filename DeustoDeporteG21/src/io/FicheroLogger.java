5e6ee62067b33d8ff7b78f218ebc0a4fc09d3cb9
ane <ane@localhost>
2023-11-21 10:37:01 +0100

    fichero

---
+++ DeustoDeporteG21/src/io/FicheroLogger.java
@@ -0,0 +1,34 @@
+package io;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class FicheroLogger {
+    private static final Logger LOGGER = Logger.getLogger(FicheroLogger.class.getName());
+    private static Handler fileHandler;
+
+    static {
+        try {
+            Handler fileHandler = new FileHandler("registroPrograma.log", true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            LOGGER.addHandler(fileHandler);
+            LOGGER.setLevel(Level.ALL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void cerrarFileHandler() {
+        if (fileHandler != null) {
+            fileHandler.close();
+        }
+    }
+    
+ 
+
+   
+}
+
+
