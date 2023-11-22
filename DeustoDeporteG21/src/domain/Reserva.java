d214ee3a4edc1212091922f4bfd0bc6e7f69d666
InigoDiazMunio <i.diazmunio@opendeusto.es>
2023-11-22 13:39:23 +0100

    clase reserva

---
+++ DeustoDeporteG21/src/domain/Reserva.java
@@ -0,0 +1,58 @@
+package domain;
+
+import java.util.Date;
+
+public class Reserva {
+	private String DNI;
+    private Actividad TipoActividad;
+    private int IDSala;
+    private Date fecha;
+    private String hora;
+	public Reserva() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Reserva(String dNI, Actividad tipoActividad, int iDSala, Date fecha, String hora) {
+		super();
+		DNI = dNI;
+		TipoActividad = tipoActividad;
+		IDSala = iDSala;
+		this.fecha = fecha;
+		this.hora = hora;
+	}
+	@Override
+	public String toString() {
+		return "Reserva [DNI=" + DNI + ", TipoActividad=" + TipoActividad + ", IDSala=" + IDSala + ", fecha=" + fecha
+				+ ", hora=" + hora + "]";
+	}
+	public String getDNI() {
+		return DNI;
+	}
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+	public Actividad getTipoActividad() {
+		return TipoActividad;
+	}
+	public void setTipoActividad(Actividad tipoActividad) {
+		TipoActividad = tipoActividad;
+	}
+	public int getIDSala() {
+		return IDSala;
+	}
+	public void setIDSala(int iDSala) {
+		IDSala = iDSala;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public String getHora() {
+		return hora;
+	}
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+}
